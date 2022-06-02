d68f107dee81132fa8e71fd227bf3062023cdd57
Mostafa Milly <https://m.milly@github.com>
2022-06-02 21:48:39 +0300

    fix width issue

--- src/pages/access/views/signup/containers/Signup.js
+++ src/pages/access/views/signup/containers/Signup.js
@@ -2,20 +2,28 @@ import { Stack, Box, TextField, Button } from "@mui/material";
 import React from "react";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import { useNavigate } from "react-router-dom";
+import { Logo } from "../../../../../common/components";
 
 function Signup() {
   const navigate = useNavigate();
   return (
     <Stack p={4} spacing={8} alignItems="center">
-      <Box display="flex" flexDirection="column" alignItems="center" rowGap={1}>
-        <img
-          src="/images/IT SMILE.svg"
-          alt="logo"
-          width="100px"
-          height="100px"
-        />
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        rowGap={1}
+        sx={{ width: "100%" }}
+      >
+        <Logo />
       </Box>
-      <Box component="form" display="flex" rowGap={4} flexDirection="column">
+      <Box
+        component="form"
+        display="flex"
+        rowGap={4}
+        flexDirection="column"
+        sx={{ width: "100%" }}
+      >
         <TextField label="اسم المستخدم" placeholder="ادخل اسم المستخدم" />
         <TextField
           label="البريد الالكتروني"
@@ -33,7 +41,7 @@ function Signup() {
           placeholder="ادخل الرقم الجامعي"
         />
       </Box>
-      <Box display="flex" columnGap={2}>
+      <Box display="flex" columnGap={2} sx={{ width: "100%" }}>
         <Button fullWidth>تسجيل حساب</Button>
         <Button
           fullWidth
