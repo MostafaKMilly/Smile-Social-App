05a42a1e3e6c1f917da7fb982ad275940fe61667
Mostafa Milly <https://m.milly@github.com>
2022-06-02 21:49:17 +0300

    finish app bar and navigtion layout

---
+++ src/layouts/components/AppBar.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { AppBar as MuiAppBar, Toolbar } from "@mui/material";
+import { Container } from "@mui/system";
+import { Logo } from "../../common/components";
+import NavigationButtons from "./NavigationButtons";
+import AppBarMenu from "./AppBarMenu";
+import { useNavigate } from "react-router-dom";
+
+function AppBar() {
+  const navigate = useNavigate();
+  return (
+    <MuiAppBar
+      position="static"
+      color="transparent"
+      elevation={0}
+      sx={{ borderBottom: "1px solid #bfccc0" }}
+    >
+      <Container maxWidth="xl">
+        <Toolbar
+          disableGutters
+          variant="dense"
+          sx={{ justifyContent: "space-between" }}
+        >
+          <Logo
+            width="60px"
+            height="60px"
+            onClick={() => navigate("/")}
+            style={{ cursor: "pointer" }}
+          />
+          <NavigationButtons />
+          <AppBarMenu />
+        </Toolbar>
+      </Container>
+    </MuiAppBar>
+  );
+}
+
+export default AppBar;
