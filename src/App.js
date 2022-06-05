473d83c5852bb68629e8ad58c4c85dacbb60cc05
Mostafa Milly <https://m.milly@github.com>
2022-06-05 21:17:44 +0300

    install apollo client

--- src/App.js
+++ src/App.js
@@ -10,24 +10,32 @@ import { CacheProvider } from "@emotion/react";
 import createCache from "@emotion/cache";
 import { prefixer } from "stylis";
 import "./global.css";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 const cacheRtl = createCache({
   key: "muirtl",
   stylisPlugins: [prefixer, rtlPlugin],
 });
 
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <CacheProvider value={cacheRtl}>
-      <Provider store={store}>
-        <ThemeProvider theme={responsiveFontSizes(theme)}>
-          <Router>
-            <CssBaseline />
-            <Routes />
-          </Router>
-        </ThemeProvider>
-      </Provider>
-    </CacheProvider>
+    <ApolloProvider client={client}>
+      <CacheProvider value={cacheRtl}>
+        <Provider store={store}>
+          <ThemeProvider theme={responsiveFontSizes(theme)}>
+            <Router>
+              <CssBaseline />
+              <Routes />
+            </Router>
+          </ThemeProvider>
+        </Provider>
+      </CacheProvider>
+    </ApolloProvider>
   );
 }
 
